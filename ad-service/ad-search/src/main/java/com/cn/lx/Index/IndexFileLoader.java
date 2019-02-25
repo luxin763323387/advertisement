b1984876bf8687f3c84577bc9415a7d868450611
StevenLu <luxin763323387@163.com>
2019-02-25 22:29:41 +0800

    init 实现全量所有的加载

---
+++ ad-service/ad-search/src/main/java/com/cn/lx/Index/IndexFileLoader.java
@@ -0,0 +1,111 @@
+package com.cn.lx.Index;
+
+import com.alibaba.fastjson.JSON;
+import com.cn.lx.dump.DConstant;
+import com.cn.lx.dump.table.*;
+import com.cn.lx.handler.AdLevelDataHandler;
+import com.cn.lx.mysql.constant.OpType;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@DependsOn("datatable")
+public class IndexFileLoader {
+
+    @PostConstruct
+    //init方法 实现全量所有的加载
+    public void init(){
+
+        List<String> adPlanStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_PLAN)
+        );
+        //遍历adPlanStrings，在便利的过程中调用AdLevelDataHandler
+        adPlanStrings.forEach(p -> AdLevelDataHandler.handleLevel2(
+                JSON.parseObject(p, AdPlanTable.class),
+                OpType.ADD
+        ));
+
+        List<String> adCreativeStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_CREATIVE)
+        );
+        adCreativeStrings.forEach(p -> AdLevelDataHandler.handleLevel2(
+                JSON.parseObject(p, AdCreativeTable.class),
+                OpType.ADD
+        ));
+
+        List<String> adUnitStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT)
+        );
+        adUnitStrings.forEach(p -> AdLevelDataHandler.handleLevel3(
+                JSON.parseObject(p, AdUnitTable.class),
+                OpType.ADD
+        ));
+
+        List<String> adCreativeUnitStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_CREATIVE_UNIT)
+        );
+        adCreativeUnitStrings.forEach(p -> AdLevelDataHandler.handleLevel3(
+                JSON.parseObject(p, AdCreativeUnitTable.class),
+                OpType.ADD
+        ));
+
+        List<String> adUnitDistrictStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT_DISTRICT)
+        );
+        adUnitDistrictStrings.forEach(p -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(p, AdUnitDiscrtTable.class),
+                OpType.ADD
+        ));
+
+        List<String> adUnitItStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT_IT)
+        );
+        adUnitItStrings.forEach(p -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(p, AdUnitItTable.class),
+                OpType.ADD
+        ));
+
+        List<String> adUnitKeywordStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT_KEYWORD)
+        );
+        adUnitKeywordStrings.forEach(p -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(p, AdUnitKeywordTable.class),
+                OpType.ADD
+        ));
+    }
+
+
+
+    //将写入到文件中的数据读取出来
+    private List<String> loadDumpData(String fileName){
+        try(BufferedReader br = Files.newBufferedReader(
+                Paths.get(fileName)
+        )){
+            return br.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
--- ad-service/ad-search/src/main/java/com/cn/lx/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/com/cn/lx/handler/AdLevelDataHandler.java
@@ -10,13 +10,18 @@ import com.cn.lx.Index.creative.CreativeIndex;
 import com.cn.lx.Index.creative.CreativeObject;
 import com.cn.lx.Index.creativeunit.CreativeUnitIndex;
 import com.cn.lx.Index.creativeunit.CreativeUnitObject;
-import com.cn.lx.dump.table.AdCreativeTable;
-import com.cn.lx.dump.table.AdCreativeUnitTable;
-import com.cn.lx.dump.table.AdPlanTable;
-import com.cn.lx.dump.table.AdUnitTable;
+import com.cn.lx.Index.district.UnitDistrictIndex;
+import com.cn.lx.Index.interest.UnitItIndex;
+import com.cn.lx.Index.keyword.UnitKeywordIndex;
+import com.cn.lx.dump.table.*;
 import com.cn.lx.mysql.constant.OpType;
 import com.cn.lx.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 1 索引之间存在着层级的划分，也就是以来关系的划分
@@ -84,7 +89,7 @@ public class AdLevelDataHandler {
             );
    }
 
-   public static void handlerLevel3(AdCreativeUnitTable adCreativeUnitTable,OpType type){
+   public static void handleLevel3(AdCreativeUnitTable adCreativeUnitTable,OpType type){
         if(type == OpType.UPDATE){
             log.error("CreativeUnitIndex not support update: {}");
             return;
@@ -114,6 +119,99 @@ public class AdLevelDataHandler {
                 creativeUnitObject,
                 type
         );
+   }
+
+   public static void handleLevel4(AdUnitDiscrtTable adUnitDiscrtTable, OpType type){
+
+        //首先地域纬度不支持更新
+        if(type == OpType.UPDATE){
+            log.error("district index can not support update");
+            return;
+        }
+
+        //获取当前系统的索引对象
+       AdUnitObject adUnitObject = DataTable.of(
+               AdUnitIndex.class
+       ).get(adUnitDiscrtTable.getUnitId());
+        //如果索引不存在，不需要创建
+        if(adUnitObject == null){
+            log.error("AdUnitDistrictTable index error: {}",adUnitDiscrtTable.getUnitId());
+            return;
+        }
+
+        //构造Key  省和市用了连字符
+        String key = CommonUtils.stringConcat(
+                adUnitDiscrtTable.getProvince(),
+                adUnitDiscrtTable.getCity()
+        );
+       Set<Long> value = new HashSet<>(
+               Collections.singleton(adUnitDiscrtTable.getUnitId())
+       );
+       handleBingLogEvent(
+               DataTable.of(UnitDistrictIndex.class),
+               key, value,
+               type);
+
+   }
+
+   public static void handleLevel4(AdUnitItTable adUnitItTable,OpType type){
+
+        //兴趣纬度不支持更新
+        if(type  == OpType.UPDATE){
+            log.error("it index can not support update");
+            return;
+        }
+
+        //获取当前所有对象
+       AdUnitObject adUnitObject = DataTable.of(
+               AdUnitIndex.class
+       ).get(adUnitItTable.getUnitId());
+        //如果所有不存在，不需要创建
+       if(adUnitObject == null){
+           log.error("adUnitTable index error: {}",adUnitItTable.getUnitId());
+           return;
+       }
+
+       Set<Long> value = new HashSet<>(
+               Collections.singleton(adUnitItTable.getUnitId())
+       );
+       handleBingLogEvent(
+               DataTable.of(UnitItIndex.class),
+               adUnitItTable.getItTag(),
+               value,
+               type
+       );
+
+   }
+
+   public static void handleLevel4(AdUnitKeywordTable adUnitKeywordTable, OpType type){
+
+        if(type == OpType.UPDATE){
+            log.error("keyword index can not support update");
+            return;
+        }
+
+        //获取当前对象
+       AdUnitObject adUnitObject = DataTable.of(
+               AdUnitIndex.class
+       ).get(adUnitKeywordTable.getUnitId());
+        //如果不存在，不需要创造
+       if(adUnitObject.getUnitId() == null){
+           log.error("AdUnitKeywordTable index error: {}",adUnitKeywordTable.getUnitId());
+           return;
+       }
+
+       Set<Long> value = new HashSet<>(
+               Collections.singleton(adUnitKeywordTable.getUnitId())
+       );
+
+       handleBingLogEvent(
+               DataTable.of(UnitKeywordIndex.class),
+               adUnitKeywordTable.getKeyword(),
+               value,
+               type
+       );
+
    }
 
     //通用方法处理全量索引，和增量索引，监听binglog
